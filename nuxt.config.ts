0adb0ce0190f873a918f85355149c5a6cabe7a27
Mackenzie C <37125149+mackiecarr89@users.noreply.github.com>
2024-02-14 11:28:00 -0400

    Added to boilerplate stuff

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  devServer: {
+    host: "dev.mackdev.ca",
+    https: {
+      cert: "./dev.mackdev.ca.pem",
+      key: "./dev.mackdev.ca-key.pem"
+    },
+    url: "https://dev.mackdev.ca:3000"
+  },
+  css: ["~/assets/css/tailwind.css", "~/assets/css/main.css"],
+  modules: [
+    "@nuxt/devtools",
+    "@nuxtjs/color-mode",
+    "@nuxtjs/google-fonts",
+    "@vueuse/nuxt",
+  ],
 })
